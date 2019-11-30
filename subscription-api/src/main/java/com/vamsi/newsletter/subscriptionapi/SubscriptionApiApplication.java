45c41246520c87b8a6573e07e57160e85020d520
Vamsi Vangapandu <vamsi020@gmail.com>
2019-11-30 10:12:57 +0530

    Initial Commit

---
+++ subscription-api/src/main/java/com/vamsi/newsletter/subscriptionapi/SubscriptionApiApplication.java
@@ -0,0 +1,13 @@
+package com.vamsi.newsletter.subscriptionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionApiApplication.class, args);
+	}
+
+}
